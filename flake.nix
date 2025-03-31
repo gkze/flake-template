087934cae9ac24208abaab973eea102f5bb69e05
gkze <george.kontridze@gmail.com>
2025-03-30 19:16:01 -0700

    updates

--- flake.nix
+++ flake.nix
@@ -1,33 +1,88 @@
 {
-  description = "Flake template";
+  description = "My Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fp.url = "github:hercules-ci/flake-parts";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    flakelight = {
+      url = "github:nix-community/flakelight";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = inputs: inputs.fp.lib.mkFlake { inherit inputs; } {
-    systems = inputs.nixpkgs.lib.systems.flakeExposed;
-
-    perSystem = { system, config, pkgs, lib, ... }: {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [ inputs.devshell.overlays.default ];
-      };
-
-      devShells.default = pkgs.devshell.mkShell {
-        env = [ ];
-        packages = with pkgs; [ ];
-      };
-    };
-
-    flake.templates.default = {
-      path = ./.;
-      description = "Minimal Nix Flake Template";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    inputs:
+    inputs.flakelight ./. (
+      { lib, ... }:
+      {
+        inherit inputs;
+        systems = [
+          "aarch64-darwin"
+          "x86_64-linux"
+        ];
+        nixpkgs.config.allowUnfree = true;
+        withOverlays = [ inputs.devshell.overlays.default ];
+
+        devShell =
+          pkgs:
+          pkgs.devshell.mkShell {
+            name = "myflake";
+
+            packages = with pkgs; [ ];
+          };
+
+        # evalModules here to pass eval'd config to formatting check below
+        # so that nix flake check passes for formatting
+        formatter =
+          pkgs:
+          with inputs.treefmt-nix.lib;
+          let
+            shInclude = [ ".envrc" ];
+            inherit
+              (evalModule pkgs {
+                projectRootFile = "flake.nix";
+                programs = {
+                  mdformat.enable = true;
+                  nixfmt.enable = true;
+                  deadnix.enable = true;
+                  prettier.enable = true;
+                  statix.enable = true;
+                  shellcheck = {
+                    enable = true;
+                    includes = shInclude;
+                  };
+                  shfmt = {
+                    enable = true;
+                    includes = shInclude;
+                  };
+                };
+              })
+              config
+              ;
+          in
+          mkWrapper pkgs (
+            config
+            // {
+              build.wrapper = pkgs.writeShellScriptBin "treefmt-nix" ''
+                exec ${config.build.wrapper}/bin/treefmt --no-cache "$@"
+              '';
+            }
+          );
+
+        checks.formatting = lib.mkForce (
+          { lib, outputs', ... }:
+          ''
+            ${lib.getExe outputs'.formatter} .
+          ''
+        );
+      }
+    );
 }
